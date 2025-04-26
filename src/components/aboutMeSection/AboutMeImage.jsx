42e5cc6fd65196c05e378c496eb858adce0eb4ed
JanardanTripathi <janardant47@gmail.com>
2025-04-26 18:22:49 +0530

    only one image is enough

--- src/components/aboutMeSection/AboutMeImage.jsx
+++ src/components/aboutMeSection/AboutMeImage.jsx
@@ -2,11 +2,11 @@ const AboutMeImage = () => {
   return (
     <div className="h-[500px] w-[300px] relative">
       <div className="h-[500px] w-[300px] rounded-[100px] absolute overflow-hidden">
-        <img
+        {/* <img
           src="/images/me.jpg"
           alt="My Image"
           className="h-full w-auto object-cover"
-        />
+        /> */}
       </div>
       <div className="h-[500px] w-[250px] bg-orange absolute bottom-[-30px] left-[-30px] rounded-bl-[120px] rounded-tr-[120px] rounded-br-[20px] rounded-tl-[20px] -z-10"></div>
     </div>
